35f8a9d2d021a1aa87756fa1788608906e5e9d5f
David A. Rios S. <64142938+riosd252@users.noreply.github.com>
2024-03-18 18:57:03 +0100

    Initial commit

---
+++ src/main/java/DavidRios/CapsHotel/CapsHotelApplication.java
@@ -0,0 +1,13 @@
+package DavidRios.CapsHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapsHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapsHotelApplication.class, args);
+	}
+
+}
