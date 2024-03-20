59067718c9e736cc9ca8ab695701204c17d68d03
David A. Rios S. <64142938+riosd252@users.noreply.github.com>
2024-03-20 11:52:47 +0100

    Adds persistence and lombok annotations to RoomType.

--- src/main/java/DavidRios/CapsHotel/entities/RoomType.java
+++ src/main/java/DavidRios/CapsHotel/entities/RoomType.java
@@ -1,6 +1,21 @@
 package DavidRios.CapsHotel.entities;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "room_types")
+@NoArgsConstructor
+@Getter
+@Setter
 public class RoomType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Setter(AccessLevel.NONE)
+    private long id;
     private String denomination;
     private String description;
 
