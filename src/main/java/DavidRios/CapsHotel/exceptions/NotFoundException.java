19ed75d45ba88b02319fcf66316e8c406c831aff
David A. Rios S. <64142938+riosd252@users.noreply.github.com>
2024-03-20 21:32:22 +0100

    Creates NotFoundException.

---
+++ src/main/java/DavidRios/CapsHotel/exceptions/NotFoundException.java
@@ -0,0 +1,8 @@
+package DavidRios.CapsHotel.exceptions;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+
+    }
+}
