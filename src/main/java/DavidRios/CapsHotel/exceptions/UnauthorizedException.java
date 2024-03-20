3eff6baae662b9564913004723ff996c9b010644
David A. Rios S. <64142938+riosd252@users.noreply.github.com>
2024-03-20 21:33:06 +0100

    Creates unauthorizedException.

---
+++ src/main/java/DavidRios/CapsHotel/exceptions/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package DavidRios.CapsHotel.exceptions;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
