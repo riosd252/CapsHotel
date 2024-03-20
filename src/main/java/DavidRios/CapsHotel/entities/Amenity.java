2e0f06ecfc9b615c0e2408516e406cc284d1ac44
David A. Rios S. <64142938+riosd252@users.noreply.github.com>
2024-03-20 11:49:50 +0100

    Adds persistence and lombok annotations to Amenity.

--- src/main/java/DavidRios/CapsHotel/entities/Amenity.java
+++ src/main/java/DavidRios/CapsHotel/entities/Amenity.java
@@ -1,6 +1,21 @@
 package DavidRios.CapsHotel.entities;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "amenities")
+@NoArgsConstructor
+@Getter
+@Setter
 public class Amenity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Setter(AccessLevel.NONE)
+    private long id;
     private String name;
     private String description;
 
