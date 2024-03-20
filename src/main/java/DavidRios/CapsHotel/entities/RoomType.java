5ac9aa7ec66a84ccd33cec53036722780f810171
David A. Rios S. <64142938+riosd252@users.noreply.github.com>
2024-03-20 11:50:37 +0100

    Creates RoomType class.

---
+++ src/main/java/DavidRios/CapsHotel/entities/RoomType.java
@@ -0,0 +1,11 @@
+package DavidRios.CapsHotel.entities;
+
+public class RoomType {
+    private String denomination;
+    private String description;
+
+    public RoomType(String denomination, String description) {
+        this.denomination = denomination;
+        this.description = description;
+    }
+}
