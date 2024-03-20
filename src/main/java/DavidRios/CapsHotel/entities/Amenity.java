5791b9b0ce2367620962356795b39c6646a81229
David A. Rios S. <64142938+riosd252@users.noreply.github.com>
2024-03-20 11:45:48 +0100

    Creates Amenity class.

---
+++ src/main/java/DavidRios/CapsHotel/entities/Amenity.java
@@ -0,0 +1,11 @@
+package DavidRios.CapsHotel.entities;
+
+public class Amenity {
+    private String name;
+    private String description;
+
+    public Amenity(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+}
