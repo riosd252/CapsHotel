3c6afb97bcf05aa0538752e1c3e8b83073cbcccc
David A. Rios S. <64142938+riosd252@users.noreply.github.com>
2024-03-20 13:44:02 +0100

    Adds persistence and lombok annotations to User entity.

--- src/main/java/DavidRios/CapsHotel/entities/User.java
+++ src/main/java/DavidRios/CapsHotel/entities/User.java
@@ -1,19 +1,33 @@
 package DavidRios.CapsHotel.entities;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.UUID;
 
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@Getter
+@Setter
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Setter(AccessLevel.NONE)
     private UUID id;
     private String name;
     private String surname;
-    private String emaill;
+    private String email;
     private String password;
     private String avatar;
 
-    public User(String name, String surname, String emaill, String password) {
+    public User(String name, String surname, String email, String password) {
         this.name = name;
         this.surname = surname;
-        this.emaill = emaill;
+        this.email = email;
         this.password = password;
     }
 }
