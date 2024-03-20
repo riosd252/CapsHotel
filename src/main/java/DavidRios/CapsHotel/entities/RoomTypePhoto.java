b2f07b8c6dd6da0240c691da84415226621cd0fb
David A. Rios S. <64142938+riosd252@users.noreply.github.com>
2024-03-20 12:30:47 +0100

    Adds persistence and lombok annotations to RoomTypePhoto and establishes a ManyToOne relationship with the RoomType entity.

--- src/main/java/DavidRios/CapsHotel/entities/RoomTypePhoto.java
+++ src/main/java/DavidRios/CapsHotel/entities/RoomTypePhoto.java
@@ -1,7 +1,23 @@
 package DavidRios.CapsHotel.entities;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "room_type_photos")
+@NoArgsConstructor
+@Getter
+@Setter
 public class RoomTypePhoto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Setter(AccessLevel.NONE)
     private long id;
+    @ManyToOne
+    @JoinColumn(name = "room_type_id")
     private RoomType roomType;
     private String photoUrl;
 
