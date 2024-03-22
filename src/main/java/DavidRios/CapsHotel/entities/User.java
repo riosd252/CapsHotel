9a252e240316d076d23718cfbd9fcd2d75ff5f4e
David A. Rios S. <64142938+riosd252@users.noreply.github.com>
2024-03-22 14:47:28 +0100

    Adds implementation of UserDetails interface to User entity.

--- src/main/java/DavidRios/CapsHotel/entities/User.java
+++ src/main/java/DavidRios/CapsHotel/entities/User.java
@@ -1,12 +1,17 @@
 package DavidRios.CapsHotel.entities;
 
 import DavidRios.CapsHotel.enums.UserRole;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
@@ -15,7 +20,8 @@ import java.util.UUID;
 @NoArgsConstructor
 @Getter
 @Setter
-public class User {
+@JsonIgnoreProperties({"password", "credentialsNonExpired", "accountNonExpired", "authorities", "accountNonLocked", "enabled"})
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Setter(AccessLevel.NONE)
@@ -25,6 +31,7 @@ public class User {
     private String email;
     private String password;
     private String avatar;
+    @Enumerated(EnumType.STRING)
     private UserRole role;
     @OneToMany(mappedBy = "user")
     private List<Booking> bookings;
@@ -34,5 +41,35 @@ public class User {
         this.surname = surname;
         this.email = email;
         this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
